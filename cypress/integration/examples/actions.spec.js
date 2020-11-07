5e001a58d04c2f4a0eb5177d3ea23f997a5c1c97
yazlai <45852211+yazlai@users.noreply.github.com>
2020-11-07 16:44:09 +0100

    Update actions.spec.js

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -24,7 +24,7 @@ context('Actions', () => {
 
       // Delay each keypress by 0.1 sec
       .type('slow.typing@email.com', { delay: 100 })
-      .should('have.value', 'slow.typing@email.comfr')
+      .should('have.value', 'slow.typing@email.com')
 
     cy.get('.action-disabled')
       // Ignore error checking prior to type
